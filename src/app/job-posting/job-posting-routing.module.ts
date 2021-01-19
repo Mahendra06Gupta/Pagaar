ece7219b0519e2d6fc3b344f68338423ec14c625
Mahendra <mtgupta@dstsystems.com>
2021-01-20 04:03:11 +0530

    bug fix

--- src/app/job-posting/job-posting-routing.module.ts
+++ src/app/job-posting/job-posting-routing.module.ts
@@ -1,29 +1,30 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { MainRoutes } from '@app/app.route-names';
 import { AuthenticatedGuard } from '@app/core/guards/authenticated.guard';
 
 import { JobPostingLandingComponent } from './components/job-posting-landing/job-posting-landing.component';
-import { JobPostingListingComponent } from './components/job-posting-listing/job-posting-listing.component';
 // import { JobPostingListingComponent } from './components/job-posting-listing/job-posting-listing.component';
 import { JobPostingComponent } from './components/job-posting/job-posting.component';
-import { EmployeeProfileRoutingPath, JobPostingTab } from './models/job-posting-routing.path';
+import { JobPostingTab } from './models/job-posting-routing.path';
 
 const JobPostingRoutes: Routes = [
   {
     path: '',
+    component: JobPostingLandingComponent,
     canActivateChild: [AuthenticatedGuard],
     children: [
+      { path: '',
+        pathMatch: 'full',
+        redirectTo: JobPostingTab.JOB_POSTING
+      },
       {
-        path: '',
-        component: JobPostingComponent,
-        children: [
-          {
-            path: EmployeeProfileRoutingPath.jobPostingList,
-            component: JobPostingListingComponent
-          }
-        ]
-      }
+        path: JobPostingTab.JOB_POSTING,
+        component: JobPostingComponent
+      },
+      // {
+      //   path: JobPostingTab.ALL_JOB_POSTING_LIST,
+      //   component: JobPostingListingComponent
+      // }
     ]
   },
 ];
